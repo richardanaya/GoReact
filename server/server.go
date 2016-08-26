88abf3c5d9afbc1be461799d054c3a4ce0680843
Richard <richard@Richards-MacBook-Pro.local>
2016-08-26 11:49:06 -0500

    adding websocket

--- server/server.go
+++ server/server.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
+)
 
 func main() {
 	r := gin.Default()
+	m := melody.New()
 	r.GET("/api/ping", pingGet)
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
 	r.Static("/static", "./static")
 	r.NoRoute(func(c *gin.Context) {
 		c.File("./client/index.html")
